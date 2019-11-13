7977c829389bf1e42ee3aa91e5329597db451812
Joy Pinckard <joypinckard@gmail.com>
2019-11-12 19:07:55 -0600

    Added SQLite Compatibility Can load items from database

--- src/Main.java
+++ src/Main.java
@@ -28,6 +28,28 @@ public class Main extends Application {
             System.out.println(spells.get(i).getName());
         }
 
+        // Add a new spell
+        Spell zoneOfTruth = new Spell(
+                "Zone of Truth",
+                "Cast a sphere in which victims must speak the truth.",
+                1,
+                1,
+                1,
+                "School",
+                "Duration",
+                "Range",
+                "Save",
+                "Components");
+
+        SQLiteHandler.AddSpell(connection, zoneOfTruth);
+        System.out.println(zoneOfTruth.getName() + " has been added to the databases.");
+
+        SQLiteHandler.Sort("spells", "id");
+
+        // Print all spells
+        for(int i = 0; i < spells.size(); i++){
+            System.out.println(spells.get(i).getName());
+        }
         launch(args);
     }
 }
--- src/SQLiteHandler.java
+++ src/SQLiteHandler.java
@@ -184,4 +184,96 @@ public class SQLiteHandler {
         return inventory;
     }
 
+    public static void AddSpell(Connection connection, Spell spell) throws Exception
+    {
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            //STEP 4: Execute a query
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+
+
+            sql = new StringBuilder().append("INSERT INTO spells (NAME, DESCRIPTION, LEVEL, PREP, USED, SCHOOL, DURATION, RANGE, SAVE, COMPONENTS) VALUES (\"").append(spell.getName()).append("\",\"").append(spell.getDescription()).append("\",").append(Integer.toString(spell.getLevel())).append(",").append(Integer.toString(spell.getPrep())).append(",").append(Integer.toString(spell.getUsed())).append(",\"").append(spell.getSchool()).append("\",\"").append(spell.getDuration()).append("\",\"").append(spell.getRange()).append("\",\"").append(spell.getSave()).append("\",\"").append(spell.getComponents()).append("\");").toString();
+
+            System.out.println("\nCommand: " + sql + "\n");
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+    }
+
+    public static void Sort(String table, String filter) throws Exception
+    {
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            //STEP 4: Execute a query
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+
+            sql = "SELECT * FROM " + table + " ORDER BY " + filter + " ASC";
+
+            System.out.println("\nCommand: " + sql + "\n");
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+    }
 }

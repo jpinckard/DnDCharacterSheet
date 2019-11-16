a3dfdaffebd73ddc9edf349b93d07c1c80c73476
jwt0004 <44759734+jwt0004@users.noreply.github.com>
2019-11-16 11:14:07 -0600

    h3

--- src/Stats.java
+++ src/Stats.java
@@ -15,8 +15,9 @@ public class Stats {
     }
 
     public void calcMod(int row){
-        int tempMod;
-
-        
+        int tempMod = statsGrid[row][1];
+        tempMod = tempMod - 10;
+        tempMod = tempMod / 2;
+        setStat(row, 1, tempMod);
     }
 }

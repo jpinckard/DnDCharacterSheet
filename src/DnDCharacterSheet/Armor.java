0870901d3729bf3aac37c82e71e54240f1b79221
Fred Snopl <fredsnopl@gmail.com>
2019-11-16 15:14:33 -0600

    Added JavaDocs

--- src/DnDCharacterSheet/Armor.java
+++ src/DnDCharacterSheet/Armor.java
@@ -1,12 +1,19 @@
 package DnDCharacterSheet;
 
+/**
+ * The armor class stores the information required from an armor
+ * item in order to determine its effectiveness. It extends the Item class
+ * to take advantage of the basic functionality of anything that can be worn or carried in the game.
+ */
 public class Armor extends Item
 {
-    private int baseAC;
-    private String aGroup;
-    private boolean stealthDisadvantage;
+    private int baseAC; // Armor provided by the armor before mods are taken into consideration
+    private String aGroup; // String equal to "light", "medium", or "heavy". This describes the classification of the armor being used.
+    private boolean stealthDisadvantage; // Some suits of armor pose a passive stealth disadvantage. If this field is true, the armor has this attribute
 
-
+    /**
+     * Sets all private variables to default values.
+     */
     public Armor()
     {
         baseAC = 10;
@@ -14,27 +21,51 @@ public class Armor extends Item
         stealthDisadvantage = false;
     }
 
-
+    /**
+     * Sets value for base AC.
+     * @param baseAC value of baseAC
+     */
     public void setBaseAC(int baseAC) {
         this.baseAC = baseAC;
     }
 
+    /**
+     * Sets armor group for later calculation.
+     * @param aGroup name of armor group
+     */
     public void setaGroup(String aGroup) {
         this.aGroup = aGroup;
     }
 
+    /**
+     * Sets stealthDisadvantage to true or false.
+     * @param stealthDisadvantage determines weather or not the armor gives disadvantage on stealth
+     */
     public void setStealthDisadvantage(boolean stealthDisadvantage) {
         this.stealthDisadvantage = stealthDisadvantage;
     }
 
+    /**
+     * Returns base armor value for armor.
+     * @return baseAC
+     */
     public int getBaseAC() {
         return baseAC;
     }
 
-    public String getaGroup() {
+    /**
+     * Returns group of armor
+     * @return aGroup armor type
+     */
+    public String getaGroup()
+    {
         return aGroup;
     }
 
+    /**
+     * Return if the armor gives disadvantage on stealth or not
+     * @return stealthDisadvantage if the armor gives disadvantage on stealth or not.
+     */
     public boolean isStealthDisadvantage() {
         return stealthDisadvantage;
     }
--- src/DnDCharacterSheet/Weapon.java
+++ src/DnDCharacterSheet/Weapon.java
@@ -1,59 +1,106 @@
 package DnDCharacterSheet;
 
+/**
+ * The Weapon class stores the attributes of a weapon that distinguish it from an item
+ * These values will be displayed to the user to let the user know how to use the weapon.
+ */
 public class Weapon extends Item
 {
-    private String damage;
-    private int range;
-    private boolean martial; //0 for simple, 1 for martial
-    private boolean ranged; //0 for melee, 1 for ranged
-    private String baseMod; //Specify if it used strength or dex.
+    private String damage; // Describes the damage of the weapon Ex. "1d6" means that a 6 sided die is rolled for damage
+    private int range; // Describes the range in feet that the weapon can be used at. 5 for melee weapons unless special
+    private boolean martial; //A martial weapon is more advanced and requires proficiency to be used. 0 for simple, 1 for martial
+    private boolean ranged; //Described weather a weapon is ranged or melee. 0 for melee, 1 for ranged
+    private String baseMod; //Specify if the stat used for attack damage is strength or dexterity based.
 
+    /**
+     * Instantiates the weapon class and sets all default values to the base for a very simple weapon.
+     */
     public Weapon()
     {
-        damage = "";
+        damage = "1d4";
         range = 0;
         martial = false;
         ranged = false;
         baseMod = "Strength";
     }
 
+    /**
+     * Sets the weapon damage.
+     * @param damage is the damage caused by the weapon
+     */
     public void setDamage(String damage) {
         this.damage = damage;
     }
 
+    /**
+     * Sets the type of baseMod.
+     * @param baseMod the modifier used by the weapon
+     */
     public void setBaseMod(String baseMod) {
         this.baseMod = baseMod;
     }
 
+    /**
+     * Sets the weapons status as martial or simple.
+     * @param martial status as martial or simple
+     */
     public void setMartial(boolean martial) {
         this.martial = martial;
     }
 
+    /**
+     * Sets the range of the weapon.
+     * @param range range of the weapon
+     */
     public void setRange(int range) {
         this.range = range;
     }
 
+    /**
+     * Sets status as ranged or not on a weapon.
+     * @param ranged status as ranged or not
+     */
     public void setRanged(boolean ranged) {
         this.ranged = ranged;
     }
 
+    /**
+     * Returns damage dealt by weapon.
+     * @return damage done by weapon
+     */
     public String getDamage()
     {
         return damage;
     }
 
+    /**
+     * Returns range of the weapon in feet.
+     * @return range of the wepaon in feet
+     */
     public int getRange() {
         return range;
     }
 
+    /**
+     * Returns if the base mod is strength or dexterity for the weapon
+     * @return strength or dexterity
+     */
     public String getBaseMod() {
         return baseMod;
     }
 
+    /**
+     * Returns weapon status as a martial weapon or not.
+     * @return true if weapon is martial
+     */
     public boolean isMartial() {
         return martial;
     }
 
+    /**
+     * Returns status as ranged weapon or not.
+     * @return true if weapon is ranged
+     */
     public boolean isRanged() {
         return ranged;
     }

a86a320dd7b64eae3b1acc4566509c6ca2b5495b
Joy Pinckard <joypinckard@gmail.com>
2019-11-16 09:51:40 -0600

    Added SQLite Compatibility Can load items from database

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,7 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 import java.sql.Connection;
 import java.util.ArrayList;
 
-public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
+public class Main {
 
     public static void main(String[] args) throws Exception {
         Item item;
@@ -50,6 +36,5 @@ public class Main extends Application {
         for(int i = 0; i < spells.size(); i++){
             System.out.println(spells.get(i).getName());
         }
-        launch(args);
     }
 }
